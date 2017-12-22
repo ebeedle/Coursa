2cad4f04a7e60d59f83bffb544de1f0e1f103a1e
ebeedle <ebeedle@ucla.edu>
2017-12-21 22:35:52 -0800

    added clearDB credentials

--- server/db/index.js
+++ server/db/index.js
@@ -1,22 +1,28 @@
 var createTables = require('./config.js');
 var mysql = require('mysql');
 const Promise = require('bluebird');
-const database = 'berkScanner';
 
+
+// const connection = mysql.createConnection({
+// 	user: 'root', 
+// 	password: '', 
+// 	database: 'berkScanner'
+// })
 
 const connection = mysql.createConnection({
-	user: 'root', 
-	password: '', 
-	database: 'berkScanner'
-})
+  host     : process.env.host,
+  user     : process.env.user,
+  password : process.env.password,
+  database : process.env.database
+});
 
 // const populateCourses = require('./seedCourses.js')
 
 const db = Promise.promisifyAll(connection, { multiArgs: true });
 
 db.connectAsync()
-  .then(() => db.queryAsync(`CREATE DATABASE IF NOT EXISTS ${database}`))
-  .then(() => db.queryAsync(`USE ${database}`))
+  .then(() => db.queryAsync(`CREATE DATABASE IF NOT EXISTS ${process.env.database}`))
+  .then(() => db.queryAsync(`USE ${process.env.database}`))
   .then(() => createTables(db))
   .catch(err => console.log(err))	
 
