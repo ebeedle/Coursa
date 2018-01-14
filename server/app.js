73e6090246c9bd153e69009f9305b38705066d61
ebeedle <ebeedle@ucla.edu>
2018-01-13 16:34:29 -0800

    switch out express-session with cookie-session

--- server/app.js
+++ server/app.js
@@ -57,7 +57,7 @@ app.use(require('cookie-parser')());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+app.use(require('cookie-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 
 // Initialize Passport and restore authentication state, if any, from the
 // session.
